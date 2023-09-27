447710cd7a3271d19a2f6bea6fbfd314b36e35a2
Lucas Vieira <lucasvbs12@gmail.com>
2023-09-27 12:30:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [listaDeTarefas, setListaDeTarefas] = useState([]);
+  const [tarefa, setTarefa] = useState("");
+
+  function adicionarNovaTarefa() {
+    setListaDeTarefas([...listaDeTarefas, { texto: tarefa, concluida: false }]);
+    setTarefa("");
+  }
+
+  function deletarTarefa(index) {
+    const tempLista = [...listaDeTarefas];
+    tempLista.splice(index, 1);
+    setListaDeTarefas(tempLista);
+  }
+
+  function alternarConclusao(index) {
+    const novasTarefas = [...listaDeTarefas];
+    novasTarefas[index].concluida = !novasTarefas[index].concluida;
+    setListaDeTarefas(novasTarefas);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        value={tarefa}
+        onChange={(value) => setTarefa(value.target.value)}
+        type="text"
+      />
+      <button className="botaoAdicionar" onClick={() => adicionarNovaTarefa()}>
+        Nova Tarefa
+      </button>
+      <ul className="lista">
+        {listaDeTarefas.map((item, index) => (
+          <li className="tarefa" key={index}>
+              {item.texto} 
+            <button onClick={() => deletarTarefa(index)}>Deletar</button>
+            <label>
+              <input
+                type="checkbox"
+                checked={item.concluida}
+                onChange={() => alternarConclusao(index)}
+                disabled={item.concluida}
+              />Concluída
+              </label>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
